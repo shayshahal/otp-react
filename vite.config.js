a0324cad83af0156d5bae158629da050d0e7d8bf
shayshahal <shay840@gmail.com>
2023-06-14 16:50:01 +0300

    .

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import basicSsl from '@vitejs/plugin-basic-ssl';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
