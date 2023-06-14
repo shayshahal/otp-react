c8a1abe2c4cf3af0c23c4b2e93093d9455015d9d
shayshahal <shay840@gmail.com>
2023-06-14 16:53:33 +0300

    .

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import basicSsl from '@vitejs/plugin-basic-ssl';
+//import basicSsl from '@vitejs/plugin-basic-ssl';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), basicSsl()],
+	plugins: [react(), /*basicSsl()*/],
 	server: {
 		open: true,
-		https: true,
+	//	https: true,
 	},
 });
